ed745746e787da90d2e414b95cda8383d8a2d536
Ramkumar <ramkumar0495@gmail.com>
2023-02-10 22:39:42 +0530

    payment

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Sample {
+	private void team1() {
+		System.out.println("team1");
+
+	}
+	private void team2() {
+		System.out.println("team2");
+
+	}
+	private void team3() {
+		System.out.println("team3");
+
+	}
+
+}
